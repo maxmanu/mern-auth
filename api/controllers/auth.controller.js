feb690f7885b844e0ac83dead885b8b06364b912
max <maxcamina@gmail.com>
2024-02-29 16:50:55 -0500

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,11 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({ username, email, password: hashedPassword });
@@ -9,6 +13,25 @@ export const signup = async (req, res) => {
     await newUser.save();
     res.status(201).json({ message: "New user created successfully" });
   } catch (error) {
-    res.status(500).json({ message: "Username or email already exists" });
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not found"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Wrong credentials"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: hashedPassword, ...rest } = validUser._doc;
+    const expiryDate = new Date(Date.now() + 3600000); // 1 hour
+    res
+      .cookie("access_token", token, { httpOnly: true , expires: expiryDate})
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
   }
 };
